5d972f617d8d2d9a03f3e7f0b1b1efd3cb512551
RickAndTired <53553229+RickAndTired@users.noreply.github.com>
2020-06-09 12:40:50 -0700

    Update ubuntu-setup-rick.sh

--- ubuntu-setup-rick.sh
+++ ubuntu-setup-rick.sh
@@ -63,6 +63,7 @@ gnome-extensions disable ubuntu-appindicators@ubuntu.com
 
 gnome-extensions disable desktop-icons@csoriano
 
+
 # Qvevri installer
 wget https://github.com/RickAndTired/Qvevri/archive/v0.1-beta.tar.gz
 tar -xzf v0.1-beta.tar.gz
@@ -70,7 +71,6 @@ sudo mv Qvevri-0.1-beta /opt/Qvevri
 cp /opt/Qvevri/qvevri.desktop ~/.local/share/applications/qvevri.desktop
 sudo ln -s /opt/Qvevri/bin/qvevri /bin/qvevri
 rm v0.1-beta.tar.gz
-
 
 # libinput-gestures install
 #sudo adduser $USER input
@@ -82,7 +82,6 @@ rm v0.1-beta.tar.gz
 #cd ..
 #rm -rf libinput-gestures/
 
-touch ~/Templates/'New Document'
 
 # Bash Aliases
 echo >> ~/.bashrc
@@ -92,9 +91,14 @@ echo "alias yt='/snap/bin/youtube-dl'" >> ~/.bashrc
 
 #sudo adduser $USER kvm
 
+touch ~/Templates/'New Document'
+
 echo Setup complete
 
 # Configurations via GUIs
+
+# Ubuntu dock
+# Firefox Files Gedit Calculator Gpodder Strawberry Discord Steam Qvevri System-Monitor Terminal
 
 # Gnome Settings
 # Keyboard shortcut
@@ -111,26 +115,6 @@ echo Setup complete
 # Shortcuts
 # Ctrl+C and Ctrl+V for Copy and Paste
 
-# Ubuntu dock
-# Firefox Files Gedit Calculator Gpodder Strawberry Discord Steam Qvevri System-Monitor Terminal
-
-# Gnome Shell
-# Accessories - Archive Manager, Calculator, Cheese, Document Scanner, Document Viewer, Files, Screenshot, Startup Disk Creator, System Monitor, Terminal, Text Editor
-# Games - Solitaire, Mahjongg, Mines, Minetest, Qvevri, Steam, Sudoku
-# Graphics - Glimpse, Image Viewer, Inkscape, Shotwell
-# Internet - Chromium, Discord, Firefox, Mumble, Telegram, Thunderbird, Transmission
-# Office - Calendar, LibreOffice
-# Sound & Video - Celluloid, gPodder, mpv, OBS, Rhythmbox, Spotify, Strawberry, Videos, VLC
-# System - dconf Editor, Disk Usage, Disks, Extensions, Firewall, Pavucontrol, Settings, Software Sources, Software Updater, Startup Applications, Synaptic, Tweaks, Ubuntu Software
-# Useless - 
-
-# /usr/share/applications/update-manager.desktop 
-# Name=Software Update
-# /usr/share/applications/software-properties-gtk.desktop
-# Name=Software Sources
-#sudo sed -i 's/Name=Software Updater/Name=Software Update/g' /usr/share/applications/update-manager.desktop
-#sudo sed -i 's/Name=Software & Updates/Name=Name=Software Sources/g' /usr/share/applications/software-properties-gtk.desktop
-
 # Files bookmarks
 # data share steamapps Games Temp
 
@@ -145,18 +129,32 @@ echo Setup complete
 # gPodder
 # celluloid --mpv-options=--speed=2.0 %U
 
-# VLC
-# .config/vlc/vlc-qt-interface.conf
-# Save a profile then replace Value
-# 7\Value="1|64;39;64;38;65;|0-5;64;3-5;4-5;64;64;10-5;20-5;19-5;64-4;33-4;34-4;42-5;37;35-5;|12;11;13;14;||0-5;64;3-5;4-5;64;37;33-5;34;25-5;35-5;"
-
 # Inkscape
 # Symbolic Icons
+
 
 # Alsamixer
 # Automute - off
 
 # rnnoise mic suppression
+
+# Pulse Audio Latency
+# Check current latency
+# pacmd list-sinks | grep latency:
+# Set latency to 50ms
+# /etc/pulse/daemon.conf
+# default-fragments = 2
+# default-fragment-size-msec =25
+
+
+# OBS Settings
+# mkv - x264 - VBR - 50000 - CRF 15-22 - Keyframe 0 - CPU superfast/ultrafast
+# Audio 44.1khz - Video Lanczos - Advanced Color Space 709 - Color Range Full
+
+# VLC
+# .config/vlc/vlc-qt-interface.conf
+# Save a profile then replace Value
+# 7\Value="1|64;39;64;38;65;|0-5;64;3-5;4-5;64;64;10-5;20-5;19-5;64-4;33-4;34-4;42-5;37;35-5;|12;11;13;14;||0-5;64;3-5;4-5;64;37;33-5;34;25-5;35-5;"
 
 # Firefox
 # toolkit.tabbox.switchByScrolling;true
@@ -176,37 +174,38 @@ echo Setup complete
 # Allow Firefox to install and run studies - Disable
 # Allow Firefox to send backlogged crash reports - Enabled
 
+
+# /usr/share/applications/update-manager.desktop 
+# Name=Software Update
+# /usr/share/applications/software-properties-gtk.desktop
+# Name=Software Sources
+#sudo sed -i 's/Name=Software Updater/Name=Software Update/g' /usr/share/applications/update-manager.desktop
+#sudo sed -i 's/Name=Software & Updates/Name=Name=Software Sources/g' /usr/share/applications/software-properties-gtk.desktop
+
 # AMD ACO - add line to bottom
 # /etc/environment
 # RADV_PERFTEST=aco
 
-# Pulse Audio Latency
-# Check current latency
-# pacmd list-sinks | grep latency:
-# Set latency to 50ms
-# /etc/pulse/daemon.conf
-# default-fragments = 2
-# default-fragment-size-msec =25
-
 # Swap file
 
-# OBS Settings
-# mkv - x264 - VBR - 50000 - CRF 15-22 - Keyframe 0 - CPU superfast/ultrafast
-# Audio 44.1khz - Video Lanczos - Advanced Color Space 709 - Color Range Full
+#sudo ln -s /opt/RetroArch/retroarch /bin/retroarch
 
 # Qvevri
 # Enable left/right panel
-# Enable libretro, dolphin, pcsx2
-
-# Dolphin fix
-#sudo ln -s /usr/lib/x86_64-linux-gnu/libsndio.so.7.0 /usr/lib/x86_64-linux-gnu/libsndio.so.6.1
-
-#sudo ln -s /opt/RetroArch/retroarch /bin/retroarch
 
 # Epic Game Store fix
 # -SkipBuildPatchPrereq -opengl
 
+# Dolphin fix
+#sudo ln -s /usr/lib/x86_64-linux-gnu/libsndio.so.7.0 /usr/lib/x86_64-linux-gnu/libsndio.so.6.1
 
 
-
-
+# Gnome Shell Applications
+# Accessories - Archive Manager, Calculator, Cheese, Document Scanner, Document Viewer, Files, Screenshot, Startup Disk Creator, System Monitor, Terminal, Text Editor
+# Games - Solitaire, Mahjongg, Mines, Minetest, Qvevri, Steam, Sudoku
+# Graphics - Glimpse, Image Viewer, Inkscape, Shotwell
+# Internet - Chromium, Discord, Firefox, Mumble, Telegram, Thunderbird, Transmission
+# Office - Calendar, LibreOffice
+# Sound & Video - Celluloid, gPodder, mpv, OBS, Rhythmbox, Spotify, Strawberry, Videos, VLC
+# System - dconf Editor, Disk Usage, Disks, Extensions, Firewall, Pavucontrol, Settings, Software Sources, Software Updater, Startup Applications, Synaptic, Tweaks, Ubuntu Software
+# Useless - 
